b727b98757a936022ad6b835d3410d495de8eead
Karen Chacón <kmchacon006@gmail.com>
2020-03-12 17:18:09 -0400

    codigo

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Hola mundo</h1>    
+</template>
+
+<script>
+// @ is an alias to /src
+//import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+ /* name: 'Home',
+  components: {
+    HelloWorld
+  }*/
+}
+</script>
